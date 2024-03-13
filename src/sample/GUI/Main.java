0eb105c1e6fa011fbc46c2c4541a505141804caf
NewPhase14 <146068269+NewPhase14@users.noreply.github.com>
2024-03-13 16:43:04 +0100

    Added icon folder with the icons

--- src/sample/GUI/Main.java
+++ src/sample/GUI/Main.java
@@ -4,6 +4,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 public class Main extends Application {
@@ -16,6 +17,8 @@ public class Main extends Application {
         FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/MainWindow.fxml"));
         Parent root = loader.load();
         primaryStage.setScene(new Scene(root));
+        primaryStage.getIcons().add(new Image("icons/CuteOtter.png"));
+        primaryStage.setTitle("Image Viewer");
         primaryStage.show();
     }
 }
