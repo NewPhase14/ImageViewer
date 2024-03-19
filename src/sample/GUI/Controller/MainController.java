850f609df415cbafae493e084e24fc23caadfbcb
FZQKhaos <casjen01@easv365.dk>
2024-03-19 18:36:56 +0100

    TopBar + Draggable

--- src/sample/GUI/Controller/MainController.java
+++ src/sample/GUI/Controller/MainController.java
@@ -1,4 +1,48 @@
 package sample.GUI.Controller;
 
-public class MainController {
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.layout.HBox;
+import javafx.stage.Stage;
+import sample.GUI.Main;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class MainController implements Initializable {
+
+    @FXML
+    private HBox hBoxTopBar;
+    private double mousePosX = 0;
+    private double mousePosY = 0;
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        draggableWindow();
+    }
+
+    public void minimizeButton(ActionEvent actionEvent) {
+        Stage s = (Stage) ((Node) actionEvent.getSource()).getScene().getWindow();
+        s.setIconified(true);
+    }
+
+    public void closeButton(ActionEvent actionEvent) {
+        Stage s = (Stage) ((Node) actionEvent.getSource()).getScene().getWindow();
+        s.close();
+    }
+
+    private void draggableWindow(){
+        hBoxTopBar.setOnMousePressed(e ->{
+            mousePosX = e.getSceneX();
+            mousePosY = e.getSceneY();
+        });
+
+        hBoxTopBar.setOnMouseDragged(e -> {
+            Main.s.setX(e.getScreenX()-mousePosX);
+            Main.s.setY(e.getScreenY()-mousePosY);
+        });
+    }
+
 }
--- src/sample/GUI/Main.java
+++ src/sample/GUI/Main.java
@@ -5,20 +5,28 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.image.Image;
+import javafx.scene.paint.Color;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 
 public class Main extends Application {
-    public static void main(String[] args) { Application.launch();
 
+    public static Stage s = new Stage();
+    public static void main(String[] args) {
+        Application.launch();
     }
 
     @Override
     public void start(Stage primaryStage) throws Exception {
         FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/MainWindow.fxml"));
         Parent root = loader.load();
-        primaryStage.setScene(new Scene(root));
+        Scene scene = new Scene(root);
+        scene.setFill(Color.TRANSPARENT);
+        primaryStage.setScene(scene);
         primaryStage.getIcons().add(new Image("icons/CuteOtter.png"));
         primaryStage.setTitle("Image Viewer");
+        primaryStage.initStyle(StageStyle.TRANSPARENT);
+        s = primaryStage;
         primaryStage.show();
     }
 }
